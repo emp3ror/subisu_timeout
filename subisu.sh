9156f7976c85c7d04b7550eeba3d43cdcd39e4b0
emp3ror <thapa.manish16@gmail.com>
2014-12-17 22:05:48 +0545

    first try with link

--- subisu.sh
+++ subisu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # ping checker tool
 
-SERVER="google.com.np"
-
+SERVER="google.com"
+SUBISULOGINIP="http://202.63.240.10/userportal/login.do?requesturi=http%3A%2F%2F1.254.254.254%2F"
+username="manish"
+password="smth"
 ping -c 1 $SERVER >/dev/null 2>&1
 if [ $? -ne 0 ] ; then #if ping exits nonzero...
     # FAILS=$[FAILS + 1]
@@ -10,3 +12,6 @@ if [ $? -ne 0 ] ; then #if ping exits nonzero...
 else
     echo "SERVER exits"
 fi
+
+# echo "type=1&username=$username&password=$password"
+curl --data "type=1&username=$username&password=$password" $SUBISULOGINIP
